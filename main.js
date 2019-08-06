48728bf28f04386902099f7091e59515ae155158
Your Name <dlinkoln99@gmail.com>
2019-08-06 15:24:56 +0300

    Create task sys

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 var app = new Vue({
   el: "#app",
   data: {
-    tasks: [{ content: "Task 1", completed: false }]
+    tasks: [{ content: "Task 1", completed: false }],
+    newTask: { content: "", completed: false, priority: 0 }
   },
   methods: {
     doTask(i) {
       this.tasks[i].completed = !this.tasks[i].completed;
+    },
+    delTask(i) {
+      this.tasks.splice(i, 1);
+    },
+    createTask() {
+      this.tasks.push(this.newTask);
     }
   }
 });
